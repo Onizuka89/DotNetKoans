9f8a1de7c47263b44137665207331c867b013b58
Hanif Jameel <hanif.jameel@btinternet.com>
2014-10-11 21:39:21 +0100

    Tidy formatting.

--- CSharp/AboutAsserts.cs
+++ CSharp/AboutAsserts.cs
@@ -4,41 +4,41 @@ namespace DotNetKoans.CSharp
 {
     public class AboutAsserts : Koan
     {
-        //We shall contemplate truth by testing reality, via asserts.
+        // We shall contemplate truth by testing reality, via asserts.
         [Koan(1)]
-        public void AssertTruth() 
+        public void AssertTruth()
         {
-			Assert.True(false); //This should be true
+            Assert.True(false); //This should be true
         }
 
-        //Enlightenment may be more easily achieved with appropriate messages
+        // Enlightenment may be more easily achieved with appropriate messages.
         [Koan(2)]
-        public void AssertTruthWithMessage() 
+        public void AssertTruthWithMessage()
         {
             Assert.True(false, "This should be true -- Please fix this");
         }
 
-        //To understand reality, we must compare our expectations against reality
+        // To understand reality, we must compare our expectations against reality
         [Koan(3)]
-        public void AssertEquality() 
+        public void AssertEquality()
         {
             var expectedValue = 3;
             var actualValue = 1 + 1;
             Assert.True(expectedValue == actualValue);
         }
 
-        //Some ways of asserting equality are better than others
+        // Some ways of asserting equality are better than others
         [Koan(4)]
-        public void ABetterWayOfAssertingEquality() 
+        public void ABetterWayOfAssertingEquality()
         {
             var expectedValue = 3;
             var actualValue = 1 + 1;
             Assert.Equal(expectedValue, actualValue);
         }
 
-        //Sometimes we will ask you to fill in the values
+        // Sometimes we will ask you to fill in the values
         [Koan(5)]
-        public void FillInValues() 
+        public void FillInValues()
         {
             Assert.Equal(FILL_ME_IN, 1 + 1);
         }
