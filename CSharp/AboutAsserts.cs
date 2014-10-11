d5eabaebb5d4df0871d8bf0f01043921f72074c4
Hanif Jameel <hanif.jameel@btinternet.com>
2014-10-11 21:42:03 +0100

    Move comments inside method blocks to be consistent with other koan files.

--- CSharp/AboutAsserts.cs
+++ CSharp/AboutAsserts.cs
@@ -4,42 +4,42 @@ namespace DotNetKoans.CSharp
 {
     public class AboutAsserts : Koan
     {
-        // We shall contemplate truth by testing reality, via asserts.
         [Koan(1)]
         public void AssertTruth()
         {
+            // We shall contemplate truth by testing reality, via asserts.
             Assert.True(false); //This should be true
         }
 
-        // Enlightenment may be more easily achieved with appropriate messages.
         [Koan(2)]
         public void AssertTruthWithMessage()
         {
+            // Enlightenment may be more easily achieved with appropriate messages.
             Assert.True(false, "This should be true -- Please fix this");
         }
 
-        // To understand reality, we must compare our expectations against reality
         [Koan(3)]
         public void AssertEquality()
         {
+            // To understand reality, we must compare our expectations against reality
             var expectedValue = 3;
             var actualValue = 1 + 1;
             Assert.True(expectedValue == actualValue);
         }
 
-        // Some ways of asserting equality are better than others
         [Koan(4)]
         public void ABetterWayOfAssertingEquality()
         {
+            // Some ways of asserting equality are better than others
             var expectedValue = 3;
             var actualValue = 1 + 1;
             Assert.Equal(expectedValue, actualValue);
         }
 
-        // Sometimes we will ask you to fill in the values
         [Koan(5)]
         public void FillInValues()
         {
+            // Sometimes we will ask you to fill in the values
             Assert.Equal(FILL_ME_IN, 1 + 1);
         }
     }
